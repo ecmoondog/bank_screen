a576b0b90f35fd3a5dc8171940da7e06392e07dc
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-03 17:11:12 -0700

    Add balances and prepare for calculation functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-  
-  
-})
+  var checking_balance = 1000;
+  $("#checking_balance").text(checking_balance);
+
+  var savings_balance = 3000;
+  $("#savings_balance").text(savings_balance);
+
+  $(ch_dep_button).on("click", deposit_in_checking);
+  function deposit_in_checking(event) {
+
+    var checking_input = $("#checking_input").val();
+    var checking_total = checking_balance + checking_input;
+
+    $("#checking_balance").text(checking_total);
+    var checking_balance = checking_total;
+
+    event.preventDefault();
+  }
+
+});
