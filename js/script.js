575c4428ebf3577810cb406541560f0a1cd31af5
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-03 17:56:47 -0700

    Add working savings and checking withdraw buttons, but no overdraft protections

--- js/script.js
+++ js/script.js
@@ -11,6 +11,17 @@ $(document).ready(function(){
     event.preventDefault();
   }
 
+  $(ch_with_button).on("click", withdraw_from_checking);
+  function withdraw_from_checking(event) {
+    
+    var checking_balance = $("#checking_balance").text();
+    var checking_input = $("#checking_input").val();
+    var checking_total = (parseInt(checking_balance) - parseInt(checking_input));
+
+    $("#checking_balance").text(checking_total);
+    event.preventDefault();
+  }
+
   $(sav_dep_button).on("click", deposit_in_savings);
   function deposit_in_savings(event) {
     
@@ -21,6 +32,18 @@ $(document).ready(function(){
     $("#savings_balance").text(savings_total);
     event.preventDefault();
   }
+
+  $(sav_with_button).on("click", withdraw_from_savings);
+  function withdraw_from_savings(event) {
+    
+    var savings_balance = $("#savings_balance").text();
+    var savings_input = $("#savings_input").val();
+    var savings_total = (parseInt(savings_balance) - parseInt(savings_input));
+
+    $("#savings_balance").text(savings_total);
+    event.preventDefault();
+  }
+
 
 
 
