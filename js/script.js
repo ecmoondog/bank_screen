1033936c676b390788cfcf3d7931f4c8bacb706f
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-03 17:52:20 -0700

    Add working savings deposit button

--- js/script.js
+++ js/script.js
@@ -2,22 +2,24 @@ $(document).ready(function(){
 
   $(ch_dep_button).on("click", deposit_in_checking);
   function deposit_in_checking(event) {
-    event.preventDefault();
+    
     var checking_balance = $("#checking_balance").text();
     var checking_input = $("#checking_input").val();
     var checking_total = (parseInt(checking_balance) + parseInt(checking_input));
 
     $("#checking_balance").text(checking_total);
+    event.preventDefault();
   }
 
   $(sav_dep_button).on("click", deposit_in_savings);
   function deposit_in_savings(event) {
-    event.preventDefault();
-    var checking_balance = $("#checking_balance").text();
-    var checking_input = $("#checking_input").val();
-    var checking_total = (parseInt(checking_balance) + parseInt(checking_input));
+    
+    var savings_balance = $("#savings_balance").text();
+    var savings_input = $("#savings_input").val();
+    var savings_total = (parseInt(savings_balance) + parseInt(savings_input));
 
-    $("#checking_balance").text(checking_total);
+    $("#savings_balance").text(savings_total);
+    event.preventDefault();
   }
 
 
