56165ad3627e2e7d7e0af0aa1c09aa7328fd49f4
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-03 17:49:55 -0700

    Add html starting balances and insert working deposit button for checking

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
-  var checking_balance = 1000;
-  $("#checking_balance").text(checking_balance);
-
-  var savings_balance = 3000;
-  $("#savings_balance").text(savings_balance);
-
   $(ch_dep_button).on("click", deposit_in_checking);
   function deposit_in_checking(event) {
-
+    event.preventDefault();
+    var checking_balance = $("#checking_balance").text();
     var checking_input = $("#checking_input").val();
-    var checking_total = checking_balance + checking_input;
+    var checking_total = (parseInt(checking_balance) + parseInt(checking_input));
 
     $("#checking_balance").text(checking_total);
-    var checking_balance = checking_total;
-
-    event.preventDefault();
   }
+
+  $(sav_dep_button).on("click", deposit_in_savings);
+  function deposit_in_savings(event) {
+    event.preventDefault();
+    var checking_balance = $("#checking_balance").text();
+    var checking_input = $("#checking_input").val();
+    var checking_total = (parseInt(checking_balance) + parseInt(checking_input));
+
+    $("#checking_balance").text(checking_total);
+  }
+
+
 
 });
