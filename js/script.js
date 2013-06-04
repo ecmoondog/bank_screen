aa2d56a8049438c61a64b99f5c4fe7315311a16d
Eric Mooney <eric.c.mooney@gmail.com>
2013-06-03 18:58:12 -0700

    Add working overdraft protection for checking account, and blocked overwithdraw from savings

--- js/script.js
+++ js/script.js
@@ -11,17 +11,6 @@ $(document).ready(function(){
     event.preventDefault();
   }
 
-  $(ch_with_button).on("click", withdraw_from_checking);
-  function withdraw_from_checking(event) {
-    
-    var checking_balance = $("#checking_balance").text();
-    var checking_input = $("#checking_input").val();
-    var checking_total = (parseInt(checking_balance) - parseInt(checking_input));
-
-    $("#checking_balance").text(checking_total);
-    event.preventDefault();
-  }
-
   $(sav_dep_button).on("click", deposit_in_savings);
   function deposit_in_savings(event) {
     
@@ -38,13 +27,47 @@ $(document).ready(function(){
     
     var savings_balance = $("#savings_balance").text();
     var savings_input = $("#savings_input").val();
-    var savings_total = (parseInt(savings_balance) - parseInt(savings_input));
+    
+    if(parseInt(savings_balance) < parseInt(savings_input)){
+      alert("You do not have enough money for that withdraw!");
+    }
+    else {
+      var savings_total = (parseInt(savings_balance) - parseInt(savings_input));
+    }
 
     $("#savings_balance").text(savings_total);
     event.preventDefault();
   }
 
+  $(ch_with_button).on("click", withdraw_from_checking);
+  function withdraw_from_checking(event) {
+    
+    var savings_balance = $("#savings_balance").text();
+    var checking_balance = $("#checking_balance").text();
+    var checking_input = $("#checking_input").val();
+    
+    if(parseInt(checking_balance) < parseInt(checking_input)){
+      if((parseInt(checking_balance) + parseInt(savings_balance)) > parseInt(checking_input)){
+        var checking_total = 0;
+        var savings_total = parseInt(savings_balance) - parseInt(checking_input);
+      }
+      else {
+        alert("You do not have enough money for that withdraw!");
+        
+      }
+    }  
+    else {
+      var checking_total = (parseInt(checking_balance) - parseInt(checking_input));
+    }
+    
+    
 
+    $("#checking_balance").text(checking_total);
+    $("#savings_balance").text(savings_total);
+
+    event.preventDefault();
+
+  };
 
 
 });
